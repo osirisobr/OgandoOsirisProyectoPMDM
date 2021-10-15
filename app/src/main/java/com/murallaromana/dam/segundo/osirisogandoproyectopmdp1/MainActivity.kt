2e20ce8aa9769476e55c5ead35f682d1f1413580
OsirisObr <osirisobr@gmail.com>
2021-10-15 12:11:15 +0200

    Proyecto Creado

---
+++ app/src/main/java/com/murallaromana/dam/segundo/osirisogandoproyectopmdp1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.murallaromana.dam.segundo.osirisogandoproyectopmdp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
