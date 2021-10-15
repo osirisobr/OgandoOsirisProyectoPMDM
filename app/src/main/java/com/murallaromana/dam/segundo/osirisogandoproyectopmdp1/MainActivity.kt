6308f32d3cde40fc4fc80d898ac6ff9eeb97a53f
OsirisObr <osirisobr@gmail.com>
2021-10-15 13:21:25 +0200

    Proyecto Primera Parte

--- app/src/main/java/com/murallaromana/dam/segundo/osirisogandoproyectopmdp1/MainActivity.kt
+++ app/src/main/java/com/murallaromana/dam/segundo/osirisogandoproyectopmdp1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.murallaromana.dam.segundo.osirisogandoproyectopmdp1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+     private lateinit var btregistrate:Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btregistrate=findViewById(R.id.btregistrate)
+        btregistrate.setOnClickListener(){
+
+            val intent = Intent(this, RegistroActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/murallaromana/dam/segundo/osirisogandoproyectopmdp1/RegistroActivity.kt
@@ -0,0 +1,11 @@
+package com.murallaromana.dam.segundo.osirisogandoproyectopmdp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_at_acceder)
+    }
+}
