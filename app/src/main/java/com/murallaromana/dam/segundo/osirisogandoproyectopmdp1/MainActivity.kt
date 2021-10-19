9401888a6dffadda1d34c6641e3227081766ebff
OsirisObr <osirisobr@gmail.com>
2021-10-19 10:04:41 +0200

    Version 2

--- app/src/main/java/com/murallaromana/dam/segundo/osirisogandoproyectopmdp1/MainActivity.kt
+++ app/src/main/java/com/murallaromana/dam/segundo/osirisogandoproyectopmdp1/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.murallaromana.dam.segundo.osirisogandoproyectopmdp1
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.media.session.MediaSession
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,7 +24,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegistroActivity::class.java)
             startActivity(intent)
 
+          //  val tvGuardarDatos = findViewById<TextView>(R.id.tvGuardarDatos)
+          //  val etEnail = findViewById<EditText>(R.id.etEnail)
+
+          //      tvGuardarDatos.setOnClickListener{
+           //         var sharedPrefs = getPreferences(Context.MODE_PRIVATE)
+           //         var editor = SharedPreferences.edit() //Iniciando transaccion
+            //        editor.putString("", etEnail.text.toString())
+
+
+
+
+
+
+                }
+
+
+
+
         }
 
     }
-}
